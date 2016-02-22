ec5855e363327bdb4e1f22c09aac752bf22f2f1d
Juan Julián Caro Sancho <carsanju@hotmail.com>
2016-02-22 15:27:45 +0100

    Redux implemented

---
+++ app/actions/notes.js
@@ -0,0 +1,29 @@
+import uuid from 'node-uuid';
+
+export const CREATE_NOTE = "CREATE_NOTE";
+
+export function createNote(note) {
+    return {
+        type: CREATE_NOTE,
+        note: {
+            id: uuid.v4(),
+            ...note
+        }
+    };
+};
+
+export const UPDATE_NOTE = 'UPDATE_NOTE';
+export function updateNote(updateNote) {
+    return {
+        type: UPDATE_NOTE,
+        ...updateNote
+    };
+};
+
+export const DELETE_NOTE = 'DELETE_NOTE';
+export function deleteNote(id) {
+    return {
+        type: DELETE_NOTE,
+        id
+    };
+};
--- app/index.jsx
+++ app/index.jsx
@@ -2,6 +2,21 @@ import './main.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
+import Root from './containers/Root.jsx';
+import configureStore from './store/configureStore';
+import storage from './libs/storage';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const APP_STORAGE = 'redux_kanban';
+
+const store = configureStore(storage.get(APP_STORAGE) || {});
+
+store.subscribe(() => {
+    if(!storage.get('debug')) {
+        storage.set(APP_STORAGE, store.getState());
+    }
+});
+
+ReactDOM.render(
+    <Root store={store} />,
+    document.getElementById('app')
+);
