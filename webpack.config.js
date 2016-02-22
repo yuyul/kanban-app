91c05e43de95f5c876735583af498ea340a30b23
Juan Julián Caro Sancho <carsanju@hotmail.com>
2016-02-22 12:28:49 +0100

    React kanban app

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
-var path = require('path');
-var HtmlwebpackPlugin = require('html-webpack-plugin');
-var merge = require('webpack-merge');
-var webpack = require('webpack');
+const path = require('path');
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+const NpmInstallPlugin = require('npm-install-webpack-plugin');
 
 
 const TARGET = process.env.npm_lifecycle_event;
@@ -10,22 +10,27 @@ const PATHS = {
     build: path.join(__dirname, 'build'),
 };
 
+process.env.BABEL_ENV = TARGET;
+
 const common = {
     entry: PATHS.app,
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
     output: {
         path: PATHS.build,
         filename: 'bundle.js',
     },
-    plugins: [
-        new HtmlwebpackPlugin({
-            title: 'Kanban app'
-        })
-    ],
     module: {
         loaders: [
             {
                 test: /\.css$/,
                 loaders: ['style', 'css'],
+                include: PATHS.app
+            },
+            {
+                test: /\.jsx?$/,
+                loaders: ['babel?cacheDirectory'],
                 include: PATHS.app
             }
         ]
@@ -36,16 +41,23 @@ if(TARGET === 'start' || !TARGET) {
     module.exports = merge(common, {
         devtool: 'eval-source-map',
         devServer: {
+            contentBase: PATHS.build,
+
             historyApiFallback: true,
             hot: true,
             inline: true,
             progress: true,
+
             stats: 'errors-only',
+
             host: process.env.HOST,
             port: process.env.PORT
         },
         plugins: [
-            new webpack.HotModuleReplacementPlugin()
+            new webpack.HotModuleReplacementPlugin(),
+            new NpmInstallPlugin({
+                save: true
+            })
         ]
     });
 }
